38891f69f3eadb4986ce58a7fac38de0d10dcefb
Sebastião <sebatrindade@hotmail.com>
2023-05-30 13:46:34 -0300

    atualização commit

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent {
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  @Input()
+  cardDescription = ""
+
+  @Input()
+  Id:string = "0"
+}
---
+++ src/app/datas/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Nova guitarra Ibanez",
+    "description":"Guitarra feita em fibra de carbono e captadores da linha premium Malagoli",
+    "photoCover":"https://http2.mlstatic.com/D_NQ_NP_652816-MLB44563497948_012021-O.jpg"
+  },
+
+  {
+    "id":"2",
+    "title":"Nova guitarra Ibanez",
+    "description":"Olá",
+    "photoCover":"https://http2.mlstatic.com/D_NQ_NP_652816-MLB44563497948_012021-O.jpg"
+  }
+]
